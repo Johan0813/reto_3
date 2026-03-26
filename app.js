c6b742ebd445ddd497112a08b1eb3033feeb9812
Johan <johan131105@gmail.com>
2026-03-26 15:47:51 -0600

    comentarios js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 function enviarFormulario(event) {
     event.preventDefault();
-
+// Evita que el formulario se envíe de forma predeterminada
     var nombre = document.getElementById("nombre").value;
     var correo = document.getElementById("correo").value;
     var identificacion = document.getElementById("identificacion").value;
 
     if (nombre == "" || correo == "" || identificacion == "") {
-        alert("Complete todos los campos");
+        alert("Complete todos los campos"); //valida que no haya campos vacíos
     } else {
-        alert("Formulario enviado");
-        document.forms[0].reset();
+        alert("Formulario enviado");//mensaje de confirmación
+        document.forms[0].reset(); //resetea el formulario después de enviarlo
     }
 }
-
+// formulario de contacto
 document.getElementsByTagName("form")[0].onsubmit = enviarFormulario;
